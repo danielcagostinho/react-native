8da68f463f30877247f777330957b81cddafa790
danielcagostinho <danielcagostinho@gmail.com>
2020-07-01 10:30:52 -0400

    Post Route

--- track/track-server/src/index.js
+++ track/track-server/src/index.js
@@ -1,6 +1,28 @@
+require('./models/user');
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const authRoutes = require('./routes/authRoutes');
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(authRoutes)
+
+var mongoUri = 'mongodb+srv://admin:passwordpassword@cluster0-lgirv.mongodb.net/<dbname>?retryWrites=true&w=majority';
+
+mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useCreateIndex: true
+});
+
+mongoose.connection.on('connected', () => {
+    console.log('Connected to mongo instance')
+});
+
+mongoose.connection.on('error', (err) => {
+    console.error('Error connecting to mongo', err)
+})
 
 app.get('/',(req, res)=> {
     res.send('Hi there!')
